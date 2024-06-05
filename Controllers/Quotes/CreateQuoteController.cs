384ffdc5d076d78f304c3009f97176fbcc312dcb
Davidhzk2 <guffyherrera150@gmail.com>
2024-06-04 20:52:42 -0500

    Confifuracion de envio de correo

--- Controllers/Quotes/CreateQuoteController.cs
+++ Controllers/Quotes/CreateQuoteController.cs
@@ -16,11 +16,13 @@ namespace PruebaDesemp.Controllers.Quotes
         private readonly IQuotesRepository _quotesRepository;
         private readonly IEmailService _emailService;
         private readonly IOwnersRepository _ownersRepository;
+        private readonly IPetsRepository _petsRepository;
 
-       public CreateQuoteController(IQuotesRepository quotesRepository, IEmailService emailService, IOwnersRepository ownersRepository){
+       public CreateQuoteController(IQuotesRepository quotesRepository, IEmailService emailService, IOwnersRepository ownersRepository, IPetsRepository petsRepository){
 
             _quotesRepository = quotesRepository;
             _ownersRepository = ownersRepository;
+            _petsRepository = petsRepository;
             _emailService = emailService;
        }
 
@@ -34,9 +36,12 @@ namespace PruebaDesemp.Controllers.Quotes
 
             try{
                 var result = await _quotesRepository.CreateQuote(quote);
+                var pet = await _petsRepository.GetPetById(result.PetId);
+                var owner = await _ownersRepository.GetOwnerById(pet.OwnerId);
 
                 //send email to  the owner !
-                var email = await _emailService.SendEmail(result, "guffyherrera150@gmail.com");
+                // var email = await _emailService.SendEmail(result, "guffyherrera150@gmail.com"); //quemado !!
+                 var email = await _emailService.SendEmail(result, owner.Email);
 
                 return CreatedAtAction(nameof(QuotesController.GetQuoteById), "Quotes", new{id = result.Id}, new{status=201, email,message= "The Quote was created Successfully!",result});
                 //return Ok(result);
